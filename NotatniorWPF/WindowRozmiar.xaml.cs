556cdd0169e69575f1a2f898c1df75e688efa67a
palstr <144432428+palstr@users.noreply.github.com>
2025-01-31 10:47:49 +0100

    Update WindowRozmiar.xaml.cs

--- NotatniorWPF/WindowRozmiar.xaml.cs
+++ NotatniorWPF/WindowRozmiar.xaml.cs
@@ -17,23 +17,57 @@ namespace NotatniorWPF
     /// <summary>
     /// Logika interakcji dla klasy WindowRozmiar.xaml
     /// </summary>
-    public partial class WindowRozmiar : Window
+   public partial class WindowRozmiar : Window
     {
         public List<int> rozmiaryCzcionek { get; set; } = new List<int>();
+        public int Rozmiar { get; set; } = 12;
+        public FontStyle Styl1 { get; set; } = FontStyles.Normal;
+        public FontWeight Styl2 { get; set; } = FontWeights.Normal;
         public WindowRozmiar()
         {
             InitializeComponent();
-            
-            for(int i=6; i>40; i += 2)
+
+            for (int i = 6; i < 40; i += 2)
             {
                 rozmiaryCzcionek.Add(i);
             }
             DataContext = this;
+
+            string podglad = podgladTextBlock.Text;
+
+            Rozmiar = 20;
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        
+
+        private void Button_Click_Zatwierdz(object sender, RoutedEventArgs e)
         {
+            Rozmiar = (int)(listVju.SelectedItem);
+            podgladTextBlock.FontSize = Rozmiar;
+            if(stylCombobox.SelectedIndex == 0)
+            {
+                Styl1 = FontStyles.Italic;
+                Styl2 = FontWeights.Normal;
+            }
+            else if(stylCombobox.SelectedIndex == 1)
+            {
+                Styl1 = FontStyles.Normal;
+                Styl2 = FontWeights.Bold;
+            }
+            else if(stylCombobox.SelectedIndex == 2)
+            {
+                Styl1 = FontStyles.Normal;
+                Styl2 = FontWeights.Normal;
+            }
+            else
+            {
+                Styl1 = FontStyles.Italic;
+                Styl2 = FontWeights.Bold;
+            }
+            podgladTextBlock.FontStyle = Styl1;
+            podgladTextBlock.FontWeight = Styl2;
 
         }
+
     }
 }
